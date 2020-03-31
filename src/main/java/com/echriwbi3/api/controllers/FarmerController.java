09740e327943d14dd5621173da630b16dd800e86
sidali001 <sidai.bougarne@gmail.com>
2020-03-31 22:45:46 +0100

    Farmmer exemple

---
+++ src/main/java/com/echriwbi3/api/controllers/FarmerController.java
@@ -0,0 +1,30 @@
+package com.echriwbi3.api.controllers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.echriwbi3.api.entity.Farmer;
+import com.echriwbi3.api.repository.FarmerRepository;
+
+@RestController
+public class FarmerController {
+	
+	@Autowired FarmerRepository farmerRepository;
+	
+	@RequestMapping(name = "/")
+	public List<Farmer> getAllFarmers(){
+		
+		List<Farmer> result = new ArrayList<Farmer>();
+		farmerRepository.findAll().forEach(result::add);
+
+		
+		
+		return result;
+		
+	}
+}
---
+++ src/main/java/com/echriwbi3/api/repository/FarmerRepository.java
@@ -0,0 +1,9 @@
+package com.echriwbi3.api.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.echriwbi3.api.entity.Farmer;
+
+public interface FarmerRepository extends CrudRepository<Farmer, Integer> {
+
+}
