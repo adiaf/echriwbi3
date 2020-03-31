b4c2e43cc2d18e10a0e6b0e0e721c16f4db96568
sidali001 <sidai.bougarne@gmail.com>
2020-04-01 00:18:34 +0100

    add @CrossOrigin

---
+++ bin/README.md
@@ -0,0 +1,1 @@
+# echriwbi3
--- src/main/java/com/echriwbi3/api/controllers/FarmerController.java
+++ src/main/java/com/echriwbi3/api/controllers/FarmerController.java
@@ -5,17 +5,20 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.echriwbi3.api.entity.Farmer;
 import com.echriwbi3.api.repository.FarmerRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class FarmerController {
 	
 	@Autowired FarmerRepository farmerRepository;
 	
+	@CrossOrigin(origins = "*")
 	@RequestMapping(name = "/")
 	public List<Farmer> getAllFarmers(){
 		
